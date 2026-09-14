51b6d1c2f481c2bc2a7a8631ee8624d00ba321ee
anne62070366 <anne62070366@L21002-185624>
2026-09-14 14:22:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as_tres_magias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pontosjogador = 0;
+            int pontoscomputador = 0;
+
+            Console.Title = "As Três Magias";
+
+            Console.WriteLine("Bem-vindo ao jogo As Três Magias!");
+
+        }
+    }
+}
